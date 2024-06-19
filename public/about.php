ee4764531e0e5a6f55683c86e15940d08fe46c93
fatima594 <fatimalkhl33@gmail.com>
2024-06-20 00:24:06 +0330

    modified file aboutus

---
+++ public/about.php
@@ -0,0 +1,16 @@
+<?PHP
+
+
+?>
+
+<html>
+<body>
+
+<form action="welcome.php" method="POST">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
